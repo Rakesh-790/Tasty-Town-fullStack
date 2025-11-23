68bb96686902340f911b4ada20c46f0cd772a486
Rakesh_790 <brakeshroshan098@gmail.com>
2025-11-23 11:30:03 +0530

    fix the toggle button in navbar

--- frontend/src/admin/components/Navbar/AdminNavbar.jsx
+++ frontend/src/admin/components/Navbar/AdminNavbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 const AdminNavbar = () => {
+
+  const toggleSidebar = () => {
+    document.body.classList.toggle("sb-sidenav-toggled");
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom shadow-lg">
-        <div className="container-fluid">
-            <i className='bi bi-list'></i>
-            <img src="/logo.png" alt="..." height={41} width={32} />
-        </div>
+      <div className="container-fluid">
+        <button className="btn" onClick={toggleSidebar}>
+          <i className="bi bi-list fs-3"></i>
+        </button>
+        <img src="/logo.png" alt="..." height={41} width={32} />
+      </div>
+      <i className='bi bi-list'></i>
     </nav>
   )
 }
