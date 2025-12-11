b53cd60672e2be4ef5b1481d226c2514cd76d7bb
Rakesh_790 <brakeshroshan098@gmail.com>
2025-12-11 21:33:35 +0530

    correction in IImageService

--- backend/src/main/java/com/tastytown/backend/service/IImageService.java
+++ backend/src/main/java/com/tastytown/backend/service/IImageService.java
@@ -1,6 +1,8 @@
 package com.tastytown.backend.service;
 
 import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
+
 
 public interface IImageService {
     String uploadFoodImage(MultipartFile file) throws IOException;
